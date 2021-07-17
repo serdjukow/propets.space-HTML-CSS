0896261437e91e673497be72f32964181aeac9e5
Sergej <62400556+serdjukow@users.noreply.github.com>
2021-07-17 10:31:16 +0200

    U-9

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 import MicroModal from 'micromodal';
 import gsap from "gsap";
 let cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree
-
+import ScrollTrigger from "scrolltrigger"
 document.addEventListener('DOMContentLoaded', () => {
 
 
@@ -31,30 +31,64 @@ document.addEventListener('DOMContentLoaded', () => {
 		)
 	}
 
-	gsap.fromTo(".button-lost", {
+
+
+	gsap.registerPlugin(ScrollTrigger);
+
+	gsap.from(".welcome__img", {
+		scrollTrigger: {
+			trigger: ".welcome__img",
+			//end: () => "+=" + document.querySelector(".welcome__img").offsetWidth,
+			//toggleActions: "restart none none none"
+		},
+		opacity: 0,
+		duration: 2,
+		scale: 0.7
+	});
+
+	gsap.from(".button-lost", {
+		scrollTrigger: {
+			trigger: ".button-lost",
+			//toggleActions: "restart pause none pause",			
+		},
 		x: -320,
-	}, {
-		x: 0,
+		duration: 1.5,
+	});
+
+
+
+	gsap.from(".button-pet", {
+		scrollTrigger: {
+			trigger: ".button-pet",
+			//toggleActions: "restart pause none pause"
+		},
+		x: -280,
+		duration: 2,
+	})
+
+	gsap.from(".block-info__img", {
+		scrollTrigger: {
+			trigger: ".block-info__img",
+			toggleActions: "restart pause none pause"
+		},
+		opacity: 0,
+		duration: 2,
+		scale: 0.7
+	});
+
+	gsap.from(".block-info__list", {
+		scrollTrigger: {
+			trigger: ".block-info__img",
+			toggleActions: "restart pause reverse pause"
+		},
+		x: 320,
 		duration: 1,
-	})
-	gsap.fromTo(".button-pet", {
-		x: -320,
-	}, {
-		x: 0,
-		duration: 2
-	})
-	gsap.fromTo(".welcome__img", {
-		opacity: 0
-	}, {
-		opacity: 1,
-		duration: 3
-	})
-	gsap.fromTo(".block-info__img", {
-		opacity: 0
-	}, {
-		opacity: 1,
-		duration: 4
-	})
+	});
+
+
+
+
+
 
 
 	const text = document.querySelector('.welcome__title');
